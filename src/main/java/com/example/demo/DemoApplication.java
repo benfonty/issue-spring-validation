7e1500a3eb2c10bd847fe32ae348eca527750524
benfonty <benfonty@gmail.com>
2025-04-10 16:01:11 +0200

    chore: init commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+@SpringBootApplication
+@EnableConfigurationProperties(MyProperties.class)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public MethodValidationPostProcessor methodValidationPostProcessor() {
+		return new MethodValidationPostProcessor();
+	}
+
+}
---
+++ src/main/java/com/example/demo/MyProperties.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotBlank;
+
+@ConfigurationProperties(prefix = "myapp")
+@Validated
+public record MyProperties(@NotBlank String property) {
+    ;
+}
